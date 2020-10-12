664a6f2031d7eee6027214e8069e5271a590bed6
Wessi <kiyako1991@gmail.com>
2020-10-12 15:35:59 +0300

    Add new java class

---
+++ Jclasses/Participants.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Participants{
+	public Participants(){
+		Vector parts = new Vector();
+	}
+	public static void main(String []args){
+		new Participants();
+	}
+}
