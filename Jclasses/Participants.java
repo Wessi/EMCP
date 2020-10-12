b054843f20cc061b577f246c0aa904618b90a860
Wessi <kiyako1991@gmail.com>
2020-10-12 16:25:40 +0300

    ArrayList included

--- Jclasses/Participants.java
+++ Jclasses/Participants.java
@@ -10,6 +10,8 @@ public class Participants{
 		while(itr.hasNext()){
 		    	System.out.println(itr.next());
 		}
+		// you can also use ArrayList to add participants
+		ArrayList ar = new Arraylist();
 	}
 	public static void main(String []args){
 		System.out.println("Participants:");
