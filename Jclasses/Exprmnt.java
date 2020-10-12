39df104214c876310e73ab57468cf104a86e54d8
Wessi <kiyako1991@gmail.com>
2020-10-12 16:45:13 +0300

    Add new file and modify existing

--- Jclasses/Exprmnt.java
+++ Jclasses/Exprmnt.java
@@ -1,22 +1,19 @@
 import java.util.*;
 
-public class Participants{
-	public Participants(){
+public class Exprmnt{
+	public Exprmnt(){
 		Vector<String> parts = new Vector<String>();
 		parts.add("Tesfaye");
 		parts.add("Bitweded");
 		parts.add("Yosef");
+		
 		Iterator itr = parts.listIterator();
 		while(itr.hasNext()){
 		    	System.out.println(itr.next());
 		}
-		// you can also use ArrayList to add participants
-		ArrayList ar = new Arraylist();
-		ArrayList<String> str = new ArrayList<String>();
 	}
 	public static void main(String []args){
-		System.out.println("Participants:");
-		new Participants();
+	    System.out.println("Participants:");
+		new Exprmnt();
 	}
 }
-
